7d0479bd2910cc8308f89b4c047abf5a9abc472c
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2020-10-14 20:39:44 +0530

    Constants

---
+++ 04.Constants/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+   const LENGTH int = 10
+   const WIDTH int = 20 
+   var area int
+
+   area = LENGTH * WIDTH
+   fmt.Printf("value of area : %d", area)   
+}
